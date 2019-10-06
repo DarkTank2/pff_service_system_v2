94fda83b869b09e20f043041fa6386ea35dc3407
DarkTank2 <x.s@gmx.at>
2019-10-06 22:18:55 +0200

    updated backend servant methods to fit all incoming types dynamically

--- backend/servant/index.js
+++ backend/servant/index.js
@@ -8,25 +8,158 @@ var router = express.Router()
 var logger = new (require('../logger'))('servant')
 var utls = require('../utilities')
 
+var productType = {
+    'food': ['Essen'],
+    'drinks': ['Trinken'],
+    'all': ['Essen', 'Trinken']
+}
+var reversedTypes = {
+    'Essen': 'food',
+    'Trinken': 'drinks'
+}
+
 router.get('/getFinished/:type', (request, response) => {
     var logPrefix = '[' + [request.method, request.url].join(' ') + ']'
     logger.info(logPrefix)
-    var type = request.params.type
-    if (type === 'food') {
-        utls.getFinished(request, 'Essen').then(data => {
-            response.status(200).send(data)
-        }).catch(err=> {
-            response.status(500).send(err)
-        })
-    } else if (type === 'drinks') {
-        utls.getFinished(request, 'Trinken').then(data => {
-            response.status(200).send(data)
-        }).catch(err=> {
-            response.status(500).send(err)
-        })
+    var types = productType[request.params.type]
+    if (types === undefined) {
+        logger.warn(logPrefix, 'Unknown type: ' + request.params.type)
+        response.status(404).send('Unknown type: ' + request.params.type)
     } else {
-        logger.error(logPrefix, 'Unknown type: ' + type)
-        response.status(500).send(new Error('Unknown type: ' + type))            
+        var promises = []
+        types.forEach(type => {
+            promises.push(new Promise((resolve, reject) => {
+                utls.getFinished(request, type).then(data => {
+                    var retData = {}
+                    retData[reversedTypes[type]] = data
+                    resolve(retData)
+                }).catch(err=> {
+                    reject(err)
+                })
+            }))
+        })
+        Promise.all(promises).then(data => {
+            response.status(200).send(data)
+        }).catch(err => {
+            response.status(500).send(err)
+        })
+    }
+})
+
+router.get('/getNotServedTables/:type', (request, response) => {
+    var logPrefix = '[' + [request.method, request.url].join(' ') + ']'
+    logger.info(logPrefix)
+    var types = productType[request.params.type]
+    if (types === undefined) {
+        logger.warn(logPrefix, 'Unknown type: ' + request.params.type)
+        response.status(404).send('Unknown type: ' + request.params.type)
+    } else {
+        var promises = []
+        types.forEach(type => {
+            promises.push(new Promise((resolve, reject) => {
+                utls.getNotServedTables(request, type).then(data => {
+                    var retData = {}
+                    retData[reversedTypes[type]] = data
+                    resolve(retData)
+                }).catch(err => {
+                    reject(err)
+                })
+            }))
+        })
+        Promise.all(promises).then(data => {
+            response.status(200).send(data)
+        }).catch(err => {
+            response.status(500).send(err)
+        })
+    }
+})
+router.get('/getNotCashedTables/:type', (request, response) => {
+    var logPrefix = '[' + [request.method, request.url].join(' ') + ']'
+    logger.info(logPrefix)
+    var types = productType[request.params.type]
+    if (types === undefined) {
+        logger.warn(logPrefix, 'Unknown type: ' + request.params.type)
+        response.status(404).send('Unknown type: ' + request.params.type)
+    } else {
+        var promises = []
+        types.forEach(type => {
+            promises.push(new Promise((resolve, reject) => {
+                utls.getNotCashedTables(request, type).then(data => {
+                    var retData = {}
+                    retData[reversedTypes[type]] = data
+                    resolve(retData)
+                }).catch(err => {
+                    reject(err)
+                })
+            }))
+        })
+        Promise.all(promises).then(data => {
+            response.status(200).send(data)
+        }).catch(err => {
+            response.status(500).send(err)
+        })
+    }
+})
+router.get('/getNotServedItemsByTable/:type/:tableId', (request, response) => {
+    var logPrefix = '[' + [request.method, request.url].join(' ') + ']'
+    logger.info(logPrefix)
+    var types = productType[request.params.type]
+    var id = Number(request.params.tableId)
+    if (!Number.isInteger(id)) {
+        logger.warn(logPrefix, 'TableId is not valid: ' + id)
+        response.status(404).send('TableId is not valid: ' + id)
+    } else if (types === undefined) {
+        logger.warn(logPrefix, 'Unknown type: ' + request.params.type)
+        response.status(404).send('Unknown type: ' + request.params.type)
+    } else {
+        var promises = []
+        types.forEach(type => {
+            promises.push(new Promise((resolve, reject) => {
+                utls.getNotServedItemsByTable(request, type, id).then(data => {
+                    var retData = {}
+                    retData[reversedTypes[type]] = data
+                    resolve(retData)
+                }).catch(err => {
+                    reject(err)
+                })
+            }))
+        })
+        Promise.all(promises).then(data => {
+            response.status(200).send(data)
+        }).catch(err => {
+            response.status(500).send(err)
+        })
+    }
+})
+router.get('/getNotCashedItemsByTable/:type/:tableId', (request, response) => {
+    var logPrefix = '[' + [request.method, request.url].join(' ') + ']'
+    logger.info(logPrefix)
+    var types = productType[request.params.type]
+    var id = Number(request.params.tableId)
+    if (!Number.isInteger(id)) {
+        logger.warn(logPrefix, 'TableId is not valid: ' + id)
+        response.status(404).send('TableId is not valid: ' + id)
+    } else if (types === undefined) {
+        logger.warn(logPrefix, 'Unknown type: ' + request.params.type)
+        response.status(404).send('Unknown type: ' + request.params.type)
+    } else {
+        var promises = []
+        types.forEach(type => {
+            promises.push(new Promise((resolve, reject) => {
+                utls.getNotCashedItemsByTable(request, type, id).then(data => {
+                    var retData = {}
+                    retData[reversedTypes[type]] = data
+                    resolve(retData)
+                }).catch(err => {
+                    reject(err)
+                })
+            }))
+        })
+        Promise.all(promises).then(data => {
+            response.status(200).send(data)
+        }).catch(err => {
+            response.status(500).send(err)
+        })
     }
 })
 
@@ -34,48 +167,33 @@ router.get('/getOrder/:id/:type', (request, response) => {
     var logPrefix = '[' + [request.method, request.url].join(' ') + ']'
     logger.info(logPrefix)
     var id = request.params.id
-    var type = request.params.type
+    var types = productType[request.params.type]
     if (id !== 'undefined') {
-        if (type === 'food') {
-            utls.getOrder(request, id, 'Essen').then(data => {
-                response.status(200).send(data)
-            }).catch(err => {
-                response.status(500).send(err)
-            })
-        } else if (type === 'drinks') {
-            utls.getOrder(request, id, 'Trinken').then(data => {
-                response.status(200).send(data)
-            }).catch(err => {
-                response.status(500).send(err)
-            })
-        } else if (type === 'all') {
+        if (types === undefined) {
+            logger.warn(logPrefix, 'Unknown type: ' + request.params.type)
+            response.status(404).send('Unknown type: ' + request.params.type)
+        } else {
             var promises = []
-            promises.push(new Promise((resolve, reject) => {
-                utls.getOrder(request, id, 'Essen').then(data => {
-                    resolve({food:data})
-                }).catch(err => {
-                    reject(err)
-                })
-            }))
-            promises.push(new Promise((resolve, reject) => {
-                utls.getOrder(request, id, 'Trinken').then(data => {
-                    resolve({drinks:data})
-                }).catch(err => {
-                    reject(err)
-                })
-            }))
+            types.forEach(type => {
+                promises.push(new Promise((resolve, reject) => {
+                    utls.getOrder(request, id, type).then(data => {
+                        var retData = {}
+                        retData[reversedTypes[type]] = data
+                        resolve(retData)
+                    }).catch(err=> {
+                        reject(err)
+                    })
+                }))
+            })
             Promise.all(promises).then(data => {
                 response.status(200).send(data)
             }).catch(err => {
                 response.status(500).send(err)
             })
-        } else {
-            logger.error(logPrefix, 'Unknown type: ' + type)
-            response.status(500).send(new Error('Unknown type: ' + type))
         }
     } else {
         logger.error(logPrefix, 'Undefined ID!')
-        response.status(500).send(new Error('Undefined ID!'))
+        response.status(500).send('Undefined ID!')
     }
 })
 
@@ -84,7 +202,7 @@ router.post('/updateServed/:id/:type', (request, response) => {
     logger.info(logPrefix)
     var id = request.params.id
     var type = request.params.type
-    request.body.name = 'served'
+    request.body.name = request.body.name || 'served'
     if (id !== 'undefined') {
         if (type === 'food') {
             utls.updateOrder(request, id, 'Essen', request.body).then(data => {
