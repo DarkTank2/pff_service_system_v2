5cac9e41c79b439026737de1cdefaf24f54a4711
DarkTank2 <x.s@gmx.at>
2020-01-22 20:43:28 +0100

    highlighting water and glasses at buffet

--- frontend/src/components/buffet/Order.vue
+++ frontend/src/components/buffet/Order.vue
@@ -19,7 +19,7 @@
                     </v-list-item>
                     <v-divider v-if="order[comment] !== ''"></v-divider>
                     <v-list-item v-for="(item, index) in order.items" :key="'order/' + type + '/' + order.idBestellung + '/' + index">
-                        <v-list-item-title>{{item.Name + ' x ' + item.Stueck}}</v-list-item-title>
+                        <v-list-item-title :class="item.Name.match(/[Gg]las/) !== null || item.Name.match(/[Ww]asser/) ? 'blue--text' : ''">{{item.Name + ' x ' + item.Stueck}}</v-list-item-title>
                     </v-list-item>
                 </v-list>
             </v-card-text>
@@ -61,6 +61,9 @@ export default {
         }
     },
     watch: {
+        
+    },
+    computed: {
         
     }
 }
